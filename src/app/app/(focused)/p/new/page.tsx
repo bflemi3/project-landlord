366d63a315fa7f50e136ecfea908800d22841aa1
Brandon <brandon.fleming@aristotle.ai>
2026-04-09 14:36:30 -0300

    refactor: convert property creation page to server component

--- src/app/app/(focused)/p/new/page.tsx
+++ src/app/app/(focused)/p/new/page.tsx
@@ -1,12 +1,5 @@
-'use client'
-
-import { FadeIn } from '@/components/fade-in'
 import { CreatePropertyFlow } from './create-property-flow'
 
 export default function NewPropertyPage() {
-  return (
-    <FadeIn className="h-full">
-      <CreatePropertyFlow />
-    </FadeIn>
-  )
+  return <CreatePropertyFlow />
 }
